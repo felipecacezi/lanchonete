2d84f50648be3c46715ec7ca81ab200fbe108792
Felipe Cacezi <felipecacezi@gmail.com>
2024-04-10 08:10:57 -0300

    refactor: traduzindo editar da categoria

--- app/Filament/Resources/CategoryResource/Pages/EditCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Actions\Action;
+use Illuminate\Support\Js;
 
 class EditCategory extends EditRecord
 {
@@ -13,7 +15,24 @@ class EditCategory extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Excluir'),
         ];
+    }
+
+    protected function getSaveFormAction(): Action
+    {
+        return Action::make('save')
+            ->label('Gravar')
+            ->submit('save')
+            ->keyBindings(['mod+s']);
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return Action::make('cancel')
+            ->label('Cancelar')
+            ->alpineClickHandler('document.referrer ? window.history.back() : (window.location.href = ' . Js::from($this->previousUrl ?? static::getResource()::getUrl()) . ')')
+            ->color('gray');
     }
 }
